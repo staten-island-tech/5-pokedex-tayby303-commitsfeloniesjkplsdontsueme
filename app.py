923e8a0f5654b36538da72edd38a0e30e8c0f97b
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-04-15 10:16:40 -0400

    commit

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-import json
-## Open the JSON file of pokemon data
-pokedex = open("./pokedex.json", encoding="utf8")
-## create variable "data" that represents the enitre pokedex list
+import json     # Open the JSON file of pokemon data
+pokedex = open("./pokedex.json", encoding="utf8")   # create variable "data" that represents the entire pokedex list
 languages=["english","japanese","chinese","french"]
-for index, item in enumerate(languages):
-    print(index, ":", item["name"])
-lang=input("Please enter the language you wish to select: ")
-# Add a language choice feature and print the pokemons name based on the user input
-data = json.load(pokedex)
-
+for index, language in enumerate(languages):
+    print(index, ':', language)
+lang=int(input("Please enter the index of the language you wish to select: "))    # Add a language choice feature and print the pokemons name based on the user input
+if lang == "Japanese":
+    for pokemon in data:
+        print(pokemon["name"]["japanese"])
+elif lang == "English":
+    for pokemon in data:
+        print(pokemon["name"]["english"])
+elif lang == "French":
+    for pokemon in data:
+        print(pokemon["name"]["french"])
+elif lang == "Chinese":
+    for pokemon in data:
+        print(pokemon["name"]["chinese"])
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
